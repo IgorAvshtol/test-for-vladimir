0d60f23be619688276a6655c270edaedda1a87ff
Igor Avshtol <iharrwe@gmail.com>
2021-09-05 23:12:08 +0300

    update port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // здесь у нас происходит импорт пакетов и определяется порт нашего сервера
 const app = express();
