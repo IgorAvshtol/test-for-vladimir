1cbf22cc8ac2e21a88ee815ab354c549c8cd9baa
Igor Avshtol <iharrwe@gmail.com>
2021-09-06 10:48:05 +0300

    update server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const port = process.env.PORT || 5000;
 
 // здесь у нас происходит импорт пакетов и определяется порт нашего сервера
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.png'));
+app.use(favicon(__dirname + '/dist/favicon.png'));
 
 //здесь наше приложение отдаёт статику
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 //простой тест сервера
 app.get('/ping', function (req, res) {
@@ -18,6 +18,6 @@ app.get('/ping', function (req, res) {
 
 //обслуживание html
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 app.listen(port);
